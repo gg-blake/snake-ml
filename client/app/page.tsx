dbb7291e229aa3bd047c753f9872a72feab67581
gg-blake <blake092202@gmail.com>
2025-02-16 23:24:32 -0500

    Add native client component to virtual DOM

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,17 @@
 "use client"
 import Image from "next/image";
 import StreamClient from "./stream";
-import Pane from "tweakpane";
+
 import { useEffect, useRef } from "react";
 import Model from "./tensorflow_model";
+import NativeClient from "./native";
 
 export default function Home() {
   
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Model />
+      <NativeClient />
     </div>
   );
 }
